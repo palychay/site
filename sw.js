9252001b521e9b3e3bc18d04976ed63d9ef7db99
palychay <118565996+palychay@users.noreply.github.com>
2023-07-18 20:46:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,3 +9,27 @@ toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });
+
+// sw.js
+self.addEventListener('install', (event) => {
+  async function buildCache() {
+    const cache = await caches.open(cacheName);
+    return cache.addAll([
+    
+      '/index.html',
+    ]);
+  }
+  event.waitUntil(buildCache());
+});
+
+self.addEventListener('fetch', (event) => {
+  async function cachedFetch(event) {
+    const cache = await caches.open(cacheName);
+    let response = await cache.match(event.request);
+    if (response) return response;
+    response = await fetch(event.request);
+    cache.put(event.request, response.clone());
+    return response;
+  }
+  event.respondWith(cachedFetch(event));
+});
